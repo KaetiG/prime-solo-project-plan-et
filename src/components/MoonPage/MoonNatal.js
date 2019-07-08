4e4b6814aff2686b8a97e116e377b4a59f59ec84
KaetiG <kaetigust@gmail.com>
2019-07-08 16:29:32 -0500

    added svg icons

--- src/components/MoonPage/MoonNatal.js
+++ src/components/MoonPage/MoonNatal.js
@@ -1,10 +1,107 @@
 import React, { Component } from 'react';
 
 class MoonNatal extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <>
-            <h1>Natal!</h1>
+                <div>
+                    <h4>Aries</h4>
+                    <p>Aries moons are the “go, go, go” people.
+                    They always want to be doing something all the time.
+                    This can translate to being impatient, and sometimes having a very short fuse with an even shorter explosion.
+                    If Aries moons aren’t constantly doing something they may become anxious and irritable.
+                    They have a need for independence and may also have many short lived crushes/relationships.
+                    A good thing to be mindful of for people with this placement is to just practice being more patient.</p>
+
+                    <h4>Taurus</h4>
+                    <p>Taurus moons are very emotionally stable and incredibly sensual people.
+                    They have a need for getting deeply in touch with themselves and with the earth.
+                    Often nature lovers, they may not be the ones to be very active outside but love to take in the scenery and just exist in nature.
+                    In bad times, Taurus moons may over indulge their senses.
+                    Taurus is a fixed sign, stubborn and unwavering.
+                    They’re resistant to change and because of this, things like breakups may not come easily.</p>
+
+                    <h4>Gemini</h4>
+                    <p>Gemini Moons are great conversationalists.
+                    They’re very talkative, charming, engaging and witty.
+                    They need a lot of mental stimulation to avoid becoming irritable.
+                    They may be sour towards loved ones but very friendly towards strangers.
+                    People with this placement should be mindful of that potential sourness to avoid seeming two-faced.
+                    Gemini Moons are also very open to talking about their feelings and the feelings of others but will have trouble actually feeling their own feelings.
+                    Instead they’ll put them at a distance; this causes other people and themselves confusion at times.</p>
+
+                    <h4>Cancer</h4>
+                    <p>Cancer Moons feel right at home in this placement since their sign is ruled by the moon.
+                    They are incredibly nurturing and very in touch with their emotions.
+                    Their nurturing nature may lead to clinginess.
+                    Cancer Moons often have a very good memory of past events, especially emotionally charged memories.
+                    This may cause them to dwell on the past if it was something that hurt them.
+                    If given security and tenderness, Cancer Moons return with warmth and protection.
+                    They yearn for peace and stability.</p>
+
+                    <h4>Leo</h4>
+                    <p>Leo Moons love attention and praise.
+                    Their emotional security is gained from praise they believe they deserve.
+                    They’re often creative and like to entertain, and absolutely do not like being ignored.
+                    If their pride is wounded, they will likely make a dramatic scene and sulk if they’re at home.
+                    Out in the world however, their self image is too important so they will not likely make a scene.
+                    They want people to think the best of them.</p>
+
+                    <h4>Virgo</h4>
+                    <p>Virgo Moons are incredibly detail oriented.
+                    They are also very critical of themselves and this can sometimes turn into extreme perfectionism.
+                    This self critiquing also causes Virgo Moons to be experts in self deprecating humor.
+                    They may be the type who cleans when stressed. They like to feel needed and useful.
+                    If they do not feel useful or needed, they become unhappy and tend to complain a lot.
+                    Virgo Moons are often shy in relationships and around new people.</p>
+
+                    <h4>Libra</h4>
+                    <p>People are drawn to Libra Moons and find them attractive to be around.
+                    They are social people who often rely on relationships to feel secure.
+                    You will probably never see a Libra Moon eating lunch alone!
+                    They’re lovers of beauty and are quick to notice flaws whether it be the world or with the people around them.
+                    They strive for harmony in every aspect of their life.</p>
+
+                    <h4>Scorpio</h4>
+                    <p>Scorpio Moons are emotionally intense.
+                    They feel their emotions very deeply in an “all or nothing” way.
+                    Their emotions with control them completely, or they have mastery over them.
+                    They can often see past someone’s emotional walls and see them for what they are at their emotional core.
+                    Transformation and rebirth are common for this placement.
+                    While Scorpio Moons can be very loyal in relationships, they can also be jealous and manipulative.</p>
+
+                    <h4>Sagittarius</h4>
+                    <p>Sagittarius Moons are optimistic and typically easy-going.
+                    They need their personal space and their freedom.
+                    If a situation becomes too intense for them, they’ll likely flee to avoid it.
+                    If they are feeling trapped, this will also cause them to flee.
+                    Sagittarius Moons are also very adaptable and spontaneous.
+                    They love to just wing it rather than getting bogged down with detailed planning.
+                    Lovers of travel and new experience, people with this placement often make wonderful teachers.</p>
+
+                    <h4>Capricorn</h4>
+                    <p>On the outside Capricorn Moons are calm and composed.
+                    They keep their emotions in check and under tight control.
+                    This may lead to seeming distant and cold towards others.
+                    They do not lack emotions, but instead keep them hidden under the surface.
+                    Capricorn Moons are ambitious but may not be much of a risk taker.
+                    They seek security and safety in life.</p>
+
+                    <h4>Aquarius</h4>
+                    <p>Aquarius Moons feel they are completely unique and are quite proud of this.
+                    They can often detach themselves from their emotions claiming they are “above” them.
+                    Humanitarian efforts speak to them although only on the broader spectrum.
+                    With people close to them they actually seemingly lack compassion and expect everyone to be as independent as they are.
+                    Aquarius Moons make wonderful friends, and tend to do better in friendships than relationships.</p>
+
+                    <h4>Pisces</h4>
+                    <p>Pisces Moons typically have excellent intuition.
+                    They are compassionate and very sensitive people.
+                    They are day dreamers and will sometimes retreat from the harshness of reality into their imagination.
+                    Because of their vast imagination, they are often creative people.
+                    Pisces Moons need time alone to recharge.
+                    They turn to music and water to heal.</p>
+                </div>
             </>
         )
     }

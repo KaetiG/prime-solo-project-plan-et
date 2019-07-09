e0b60fe6396ef90ffad23ce3ee3a364de10bddd5
KaetiG <kaetigust@gmail.com>
2019-07-09 09:53:55 -0500

    add svg icons to profile page

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 
+import  { ReactComponent as Sun } from '../icons/sun2.svg';
 import  { ReactComponent as Moon } from '../icons/moon.svg';
+import  { ReactComponent as Rising } from '../icons/caret-symbol.svg';
+import  { ReactComponent as Mercury } from '../icons/mercury.svg';
+import  { ReactComponent as Venus } from '../icons/venus.svg';
+import  { ReactComponent as Mars } from '../icons/mars.svg';
+import  { ReactComponent as Jupiter } from '../icons/jupiter.svg';
+import  { ReactComponent as Saturn } from '../icons/saturn-sign.svg';
+import  { ReactComponent as Neptune } from '../icons/neptune.svg';
+import  { ReactComponent as Uranus } from '../icons/uranus-sign.svg';
+import  { ReactComponent as Pluto } from '../icons/pluto.svg';
 
 
 // this could also be written with destructuring parameters as:
@@ -14,35 +24,43 @@ class ProfilePage extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="profileDiv">
         <h1 id="welcome">
           Welcome, {this.props.user.username}!
           </h1>
       <div>
             {this.props.natalReducer.map((description) => <div key={description.id}>
-           
+            <Sun height='80px'/> 
               <h3>Sun</h3>
               <p>{description.description_sun}</p>
               <Moon height='80px'/>
               <h3>Moon</h3>
               <p>{description.description_moon}</p>
-
+              <Rising height='80px'/>
               <h3>Ascendent/Rising</h3>
               <p>{description.description_asc}</p>
+              <Mercury height='80px'/>
               <h3>Mercury</h3>
               <p>{description.description_mercury}</p>
+              <Venus height='80px'/>
               <h3>Venus</h3>
               <p>{description.description_venus}</p>
+              <Mars height='80px'/>
               <h3>Mars</h3>
               <p>{description.description_mars}</p>
+              <Jupiter height='80px'/>
               <h3>Jupiter</h3>
               <p>{description.description_jupiter}</p>
+              <Saturn height='80px'/>
               <h3>Saturn</h3>
               <p>{description.description_saturn}</p>
+              <Neptune height='80px'/>
               <h3>Neptune</h3>
               <p>{description.description_neptune}</p>
+              <Uranus height='80px'/>
               <h3>Uranus</h3>
               <p>{description.description_uranus}</p>
+              <Pluto height='80px'/>
               <h3>Pluto</h3>
               <p>{description.description_pluto}</p>
               
